11c446ba62d83044ed3da80a03a176c640dd0a4e
insockjang <ijang@belltown.fhcr.org>
2014-08-18 16:39:05 -0700

    linear regression

---
+++ R5/myLMModel1.R
@@ -0,0 +1,32 @@
+myLMModel1 <- setRefClass(Class = "myLMModel1",
+                            contains="PredictiveModel",
+                            fields="model",
+                            methods = list(
+                              initialize = function(...){
+                                return(.self)
+                              },
+                              
+                              rawModel = function(){
+                                return(.self$model)
+                              },
+                              
+                              customTrain = function(featureData, responseData,...){
+                                
+                                  .self$model <- lm(responseData ~ featureData,...) 
+                                  
+                                }
+                                
+                              },
+                              
+                              customPredict = function(featureData){
+                                predictedResponse <- predict(.self$model, featureData)
+                                return(predictedResponse)
+                              },
+                              
+                              getCoefficients = function(){
+                                return(coef(.self$model))
+                              }
+                              
+                            )
+                            
+)
